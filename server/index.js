6dc416e5b3664ed9a511a760b44ac4578eb0d76e
barath <barath2112000@gmail.com>
2024-07-12 13:39:15 +0530

    add secure path

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import ENV from "dotenv";
 import { AccountRouter } from "./View/AccountRoute.js";
 import { FormRoutes } from "./View/FormRoute.js";
+import path from "path";
 
 ENV.config();
 
@@ -12,10 +13,20 @@ app.use(cors());
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use("/User", AccountRouter);
-app.use("/FormRoutes",FormRoutes)
+app.use("/FormRoutes", FormRoutes);
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URL;
+
+const __dirname = path.resolve();
+
+// Serve static files from the 'client/dist' directory
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+// Serve the 'index.html' file for all routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 mongoose
   .connect(MONGO_URL)
