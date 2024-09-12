173e79b8b3f40f417af4d8306e3477e16ff6941b
barath <barath2112000@gmail.com>
2024-09-12 23:30:37 +0530

    Your commit message

--- client/src/contribute/Page1.jsx
+++ client/src/contribute/Page1.jsx
@@ -151,7 +151,7 @@ const Page1 = () => {
               id="name"
               type="text"
               className="bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 flex-grow"
-              value="John Doe"
+              value="John"
               disabled
               readOnly
             />
