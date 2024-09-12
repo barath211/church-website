6061c9ae1eb225d17c3d2013b9f104d0a712a23f
barath <barath2112000@gmail.com>
2024-09-12 11:24:14 +0530

    major changes

--- client/src/youthministry/Bibleverse.jsx
+++ client/src/youthministry/Bibleverse.jsx
@@ -5,7 +5,7 @@ const Bibleverse = () => {
     <div className='flex flex-col md:flex-row items-center justify-center p-4'>
         {/* left side */}
         <div className=' w-full md:w-1/2 p/4'>
-     <h1 className='font-semibold text-xl text-gray-600'>"Don't let anyone look down on you because you are young, but set an example for the believers
+     <h1 className='font-semibold text-xl text-gray-600 italic'>"Don't let anyone look down on you because you are young, but set an example for the believers
          in speech, in conduct, in love, in faith, and in purity."
          <span>--Timothy 4:12</span></h1>
     </div>
