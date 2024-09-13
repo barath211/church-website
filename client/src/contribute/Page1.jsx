5a0c151ad6cdd0355a3ca268424f6122a9679da0
barath <barath2112000@gmail.com>
2024-09-14 00:00:56 +0530

    Describe your changes here

--- client/src/contribute/Page1.jsx
+++ client/src/contribute/Page1.jsx
@@ -37,7 +37,7 @@ const Page1 = () => {
 
           {/* Centered Title for Account Transfer */}
           <h3 className="text-2xl font-semibold text-gray-800 text-center mt-8 mb-4">
-            ACCOUNT TRANSFER
+            BANK TRANSFER
           </h3>
 
           {/* Account Holder Name Section */}
